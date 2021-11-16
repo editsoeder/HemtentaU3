b0d045e6b2db47f462afd0a2439eb7bf10fbe4bc
Kim <kimpfimp@posteo.net>
2021-11-16 16:23:21 +0100

    adding comments and finding Index function

--- functions.php
+++ functions.php
@@ -1,5 +1,19 @@
 <?php
 
+// Opens json file and decodes it.
+// Returns an associative array.
 function getJSON($filename){
     return json_decode(file_get_contents($filename), true);
+}
+
+/* 
+Searches array for ID.
+Returns index if found
+Returns false if no ID found.
+OBS! Index 0 is NOT false
+*/
+function getIndexOfID($data, $id){
+    $column = array_column($data, "id");
+    $index = array_search($id, $column);
+    return $index;
 }
