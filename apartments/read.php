4139cb6b6ac92df6a81b243107307f7fe4978b03
Kim <kimpfimp@posteo.net>
2021-11-16 18:09:23 +0100

    adding function for limitingResults

--- apartments/read.php
+++ apartments/read.php
@@ -1,1 +1,12 @@
+<?php
+include_once "../functions.php";
 
+/*
+Takes an integer and a data array.
+Reduces amount of array entries down to given number.
+Returns shortened array.
+*/
+function limitResult( $number, $data ){
+    $sliced = array_slice($data, 0, $number );
+    return $sliced;
+}
