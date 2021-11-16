a304f40a318c93f3545ee66725bbacf25cf1917c
Kim <kimpfimp@posteo.net>
2021-11-16 18:00:22 +0100

    getting entry from ID

--- apartments/read.php
+++ apartments/read.php
@@ -1,1 +1,13 @@
+<?php
+include_once "../functions.php";
 
+/*
+Gets an entry from specified file by given ID.
+Returns the specific entry if found.
+Returns NULL if no entry found.
+*/
+function getEntry($filename, $id){
+    $data = getJSON($filename);
+    $index = getIndexOfID($data, $id);
+    return $data[$index];
+}
