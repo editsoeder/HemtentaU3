b9d803bca11809e74fe54946f23f7f0816f80376
Kim <kimpfimp@posteo.net>
2021-11-17 15:16:01 +0100

    moving functions to functions.php

--- apartments/read.php
+++ apartments/read.php
@@ -1,64 +1,22 @@
 <?php
 include_once "../functions.php";
 
-/* Gets an entry from specified file by given ID.
-Returns the specific entry if found.
-Returns NULL if no entry found.
-*/
-function getEntry($filename, $id){
-    $data = getJSON($filename);
-    $index = getIndexOfID($data, $id);
-    return $data[$index];
-}
+function containsParam( $param ){
+    $dbKeys = array_keys(getJSON("apartments.json")[0]);
 
-/* Takes an integer and a data array.
-Reduces amount of array entries down to given number.
-Returns shortened array.
-*/
-function limitResult( $number, $data ){
-    $sliced = array_slice($data, 0, $number );
-    return $sliced;
-}
+    
 
-/* 
-Takes an array with data and returns highest ID.
-If data arary is empty, returns 0.
-*/
+    var_dump(stripos("rooms", $param));
 
-function getMaxID( $data ){
-    if( count($data) < 1 ) {
-        return 0;
+    if( $param == 'ids' ){
+
     }
-    $column = array_column($data, "id");
-    $maxID = max($column);
-    return $maxID;
-}
+    if( $param == 'include' ){
 
-/*
-Takes a key and corresponding value (eg "rooms", 2) and data array.
-Returns an array with data that has that value in that key.
-*/
-function filterBy( $key, $value, $data){
-    $filtered = array_filter( $data, function($entry) use ($key, $value) {
-            return $entry[$key] == $value;
-        }
-    );
-    return $filtered;
-}
-
-/*
-Takes the apartment array that should include tenants.
-Finds each tenant and pushes it inside the apartments array.
-Returns a new array with included tenants.
-*/
-function includeRelation( $apartments ){
-    foreach( $apartments as $key=>$apartment ){
-        $included = [];
-        foreach( $apartment["tenants"] as $tenantID ){
-            $tenant = getEntry( "../tenants/tenants.json", $tenantID );
-            array_push($included, $tenant);
-        }
-        $apartments[$key]["tenants"] = $included;
     }
-    return $apartments;
+    if( $param == 'limit' ){
+
+    }
 }
+
+containsParam( "rooms" );
