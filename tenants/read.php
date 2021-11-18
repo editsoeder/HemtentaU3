c64816ba8231e724274103ef94e9601f4ed8b558
Kim <kimpfimp@posteo.net>
2021-11-18 11:39:04 +0100

    adding read.php page

--- tenants/read.php
+++ tenants/read.php
@@ -1,1 +1,42 @@
+<?php
+require_once "../functions.php";
 
+error_reporting(-1);
+
+$directory = getDirectory();
+
+if ( isMethod("GET") ) {
+    $data = [];
+    if( containsParam( "id" ) ){
+        $id = $_GET["id"];
+        $data = getEntry( "$directory.json", $id );
+    }elseif( containsParam( "ids" ) ){
+        $ids = explode(",", $_GET["ids"]);
+        foreach( $ids as $id ){
+            array_push( $data, getEntry( "$directory.json", $id ) );
+        }
+    }else{
+        $data = getJSON("$directory.json");
+    }
+
+    $dataKeys = array_keys($data[0]);
+
+    if( containsParam( "limit" ) ){
+        $limit = $_GET["limit"];
+        $data = limitResult($limit, $data);
+    }
+
+    foreach( $dataKeys as $key ){
+        if( $key == "id" ){
+            continue;
+        }
+        if( containsParam( $key ) ){
+            $value = $_GET[$key];
+            $data = filterBy($key, $value, $data );
+        }
+    }
+
+    sendJSON( [ $directory=>$data ] , 200);
+} else{
+    sendJSON(["message"=>"Method not allowed"], 400);
+}
